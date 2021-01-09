f2408ccaf261aa643ac87045932086853c90c6da
CamaelGG <tom.maillard@protonmail.com>
2021-01-09 17:12:54 +0100

    Added EJS engine + Added deck imports

--- routes/builder.js
+++ routes/builder.js
@@ -1,7 +1,13 @@
 'use strict';
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var path = require("path")
+var util = require('util')
+
+var multer = require('multer')
+
+const upload = multer({ storage: multer.memoryStorage() })
+
 
 /* GET users listing. */
 router.get('/', function (req, res) {
@@ -13,7 +19,32 @@ router.get('/', function (req, res) {
     else {
         req.session.ydk.push('1')
     }*/
-    res.sendFile(path.join(__dirname,'../views/skull-deckbuilder.html'));
+    res.sendFile(path.join(__dirname, '../public', 'skull-deckbuilder.html'));
 });
+
+router.post('/', upload.single('file'), function (req, res) {
+    try {
+        data = String(req.file.buffer)
+        var data = data.split(/\r?\n|\r/).map(x => x.trim()).filter(id => id.length>0)
+        console.log(util.inspect(data, { maxArrayLength: null }))
+
+        var extraSeparator = data.indexOf("#extra")
+        var sideSeparator = data.indexOf("!side")
+        var main = data.slice(1, extraSeparator)
+        var extra = data.slice(extraSeparator + 1, sideSeparator)
+        var side = data.slice(sideSeparator + 1, data.length)
+    } catch (error) {
+        console.log(error)
+    }
+
+    console.log(main)
+    console.log(extra)
+    console.log(side)
+    try {
+        res.render('deckbuilder', { mainCards: main, sideCards: side, extraCards: extra })
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 'use strict';
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/', 'index.html'));
 });
 
 module.exports = router;
